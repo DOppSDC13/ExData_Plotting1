7951a9457ee47b51431ec1def21c9b6cb9d5594e
DOppSDC13 <david.opp.11@gmail.com>
2015-08-08 23:05:57 -0400

    add files for plot3 and plot4

--- plot3.R
+++ plot3.R
@@ -18,7 +18,7 @@ with(dataFeb07, {
   lines(Sub_metering_2~Datetime,col='Red')
   lines(Sub_metering_3~Datetime,col='Blue')
 })
-legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, cex=0.6, title.adj=0.15, 
        legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 ## Saving to file
